769d25720f6a792c3d2a4da58a1d5ab2c52ac651
Nathan <nathan@example.com>
2026-04-05 12:02:25 +0000

    Add proper metadata to layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,18 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'MonetCord',
+  description: ' Discord Monetization Bot',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="fr">
       <body>{children}</body>
     </html>
-  );
+  )
 }
